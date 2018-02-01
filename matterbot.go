951bffd43b4c4281190fd08de1cea076c055b494
Jakub Filak <filak.jakub@gmail.com>
2018-02-02 00:59:53 +0100

    Add a simple stupid handling of messages

--- matterbot.go
+++ matterbot.go
@@ -5,6 +5,7 @@ import (
   "fmt"
   "flag"
   "syscall"
+  "strings"
 
   "github.com/mattermost/mattermost-server/model"
 
@@ -75,22 +76,76 @@ func main() {
         os.Exit(1)
     }
 
-    post := &model.Post{}
-    post.ChannelId = channel.Id
-    post.Message = "Hello, world!"
-
-    if _, resp := Client.CreatePost(post); resp.Error != nil {
-        fmt.Println("Failed to post the message")
-        PrintServerError(resp.Error)
+    if nil != PostMessageToChannel(Client, channel.Id, "Hello, world!") {
         os.Exit(1)
     }
 
     fmt.Println("Posted!")
+
+    // Replace https with ws
+    parts := strings.Split(*serverPtr, ":")
+    parts[0] = "ws"
+    wsAddress := strings.Join(parts, ":")
+
+    webSocketClient, err := model.NewWebSocketClient4(wsAddress, Client.AuthToken)
+
+    if err != nil {
+        fmt.Printf("Failed to connects WebSocket at '%s'\n\n", wsAddress)
+        PrintServerError(err)
+        os.Exit(1)
+    }
+
+    webSocketClient.Listen()
+
+    go func() {
+        for {
+            select {
+                case resp := <-webSocketClient.EventChannel:
+                    HandleWebSocketResponse(Client, channel.Id, resp)
+           }
+        }
+    }()
+
+    select {}
 }
+
 
 func PrintServerError(err *model.AppError) {
 	fmt.Println("\tError Details:")
 	fmt.Println("\t\t" + err.Message)
 	fmt.Println("\t\t" + err.Id)
 	fmt.Println("\t\t" + err.DetailedError)
+}
+
+func PostMessageToChannel(client *model.Client4, channelId string, msg string) *model.AppError {
+    post := &model.Post{}
+    post.ChannelId = channelId
+    post.Message = msg
+
+    if _, resp := client.CreatePost(post); resp.Error != nil {
+        fmt.Println("Failed to post the message")
+        PrintServerError(resp.Error)
+        return resp.Error
+    }
+
+    return nil
+}
+
+func HandleWebSocketResponse(client *model.Client4, channelId string, event *model.WebSocketEvent) {
+    if event.Broadcast.ChannelId != channelId {
+        return
+    }
+
+    if event.Event != model.WEBSOCKET_EVENT_POSTED {
+        return
+    }
+
+    post := model.PostFromJson(strings.NewReader(event.Data["post"].(string)))
+
+    if post != nil {
+        if post.Message != "Thank you!" {
+            fmt.Println(post.Message)
+            PostMessageToChannel(client, channelId, "Thank you!")
+        }
+    }
 }
